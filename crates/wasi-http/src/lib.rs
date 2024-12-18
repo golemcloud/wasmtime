0318406d93b85be10834e57320f582ddca701974
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2024-12-18 17:30:24 +0100

    Async incoming-body::finish

--- crates/wasi-http/src/lib.rs
+++ crates/wasi-http/src/lib.rs
@@ -239,6 +239,7 @@ pub mod bindings {
                 "handle",
                 "[method]future-incoming-response.get",
                 "[method]future-trailers.get",
+                "[static]incoming-body.finish",
                 "[drop]incoming-body",
                 "[drop]incoming-response",
                 "[drop]future-incoming-response",
--- crates/wasi-http/src/types_impl.rs
+++ crates/wasi-http/src/types_impl.rs
@@ -718,7 +718,7 @@ where
         Ok(Err(()))
     }
 
-    fn finish(
+    async fn finish(
         &mut self,
         id: Resource<HostIncomingBody>,
     ) -> wasmtime::Result<Resource<HostFutureTrailers>> {
